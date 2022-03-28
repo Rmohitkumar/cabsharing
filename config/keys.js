6abc7be2d454782e2892fe71edb8b9fb99f920fc
Rmohitkumar <its2002mohit@gmail.com>
2022-03-28 14:45:27 +0530

    all commit

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+dbPassword = 'mongodb+srv://its2002monu:rmy2632766@cluster0.7ydkt.mongodb.net/cabDB'
+// mongodb+srv://its2002monu:<password>@cluster0.7ydkt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority;
+
+module.exports = {
+    mongoURI: dbPassword
+};
